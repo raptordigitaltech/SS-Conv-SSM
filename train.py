404e626b022480a25c9241a595e1931800c78450
raptordigitaltech <167582938+raptordigitaltech@users.noreply.github.com>
2026-02-16 07:55:02 -0500

    fixed parenthesis around transforms

--- train.py
+++ train.py
@@ -19,10 +19,10 @@ def main():
         "train": transforms.Compose([transforms.Resize((224, 224)),
                                      transforms.RandomHorizontalFlip(),
                                      transforms.ToTensor(),
-                                     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                                     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]),
         "val": transforms.Compose([transforms.Resize((224, 224)),
                                    transforms.ToTensor(),
-                                   transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]))}
+                                   transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])}
 
     DATA_SET_PATH = '/kaggle/input/alzheimersoriginaldataset/OriginalDataset'
 
