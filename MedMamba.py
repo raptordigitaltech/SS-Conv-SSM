7f916801dc67c08670dfe9f95367641e8e5a1fb1
raptordigitaltech <167582938+raptordigitaltech@users.noreply.github.com>
2026-02-19 19:04:52 -0500

    Did gemini miss one change of in_chans

--- MedMamba.py
+++ MedMamba.py
@@ -660,7 +660,7 @@ class VSSLayer_up(nn.Module):
 
 
 class VSSM(nn.Module):
-    def __init__(self, patch_size=4, in_chans=3, num_classes=1000, depths=[2, 2, 4, 2], depths_decoder=[2, 9, 2, 2],
+    def __init__(self, patch_size=4, in_chans=1, num_classes=1000, depths=[2, 2, 4, 2], depths_decoder=[2, 9, 2, 2],
                  dims=[96,192,384,768], dims_decoder=[768, 384, 192, 96], d_state=16, drop_rate=0., attn_drop_rate=0., drop_path_rate=0.1,
                  norm_layer=nn.LayerNorm, patch_norm=True,
                  use_checkpoint=False, **kwargs):
