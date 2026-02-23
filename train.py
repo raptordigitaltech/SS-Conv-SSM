dda6f84513ba8c3a9ee545aab2693b555e96e0be
raptordigitaltech <167582938+raptordigitaltech@users.noreply.github.com>
2026-02-22 22:19:37 -0500

    Calc mean of Image RGB and reassign to components

--- train.py
+++ train.py
@@ -93,6 +93,13 @@ def main():
         train_bar = tqdm(train_loader, file=sys.stdout)
         for step, data in enumerate(train_bar):
             images, labels = data
+
+            for image in images
+                img_gray_avg = image.mean(axis =0, keepdim=True)
+                image[0,:,:] = img_gray_avg 
+                image[1,:,:] = img_gray_avg 
+                image[2,:,:] = img_gray_avg 
+            
             optimizer.zero_grad()
             outputs = net(images.to(device))
             loss = loss_function(outputs, labels.to(device))
@@ -113,6 +120,13 @@ def main():
             val_bar = tqdm(validate_loader, file=sys.stdout)
             for val_data in val_bar:
                 val_images, val_labels = val_data
+
+                for image in val_images
+                    img_gray_avg = image.mean(axis =0, keepdim=True)
+                    image[0,:,:] = img_gray_avg 
+                    image[1,:,:] = img_gray_avg 
+                    image[2,:,:] = img_gray_avg 
+                
                 outputs = net(val_images.to(device))
                 predict_y = torch.max(outputs, dim=1)[1]
                 acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
