8a1c5aea5d0fafea42629a47b410be4e1052a60c
raptordigitaltech <167582938+raptordigitaltech@users.noreply.github.com>
2026-04-04 16:28:29 -0400

    Only 25 epochs

--- train.py
+++ train.py
@@ -91,7 +91,7 @@ def main():
     
     summary(net, input_size=(1, 3, 224, 224))
     
-    epochs = 100
+    epochs = 25
     best_acc = 0.0
     save_path = './{}Net.pth'.format(model_name)
     train_steps = len(train_loader)
